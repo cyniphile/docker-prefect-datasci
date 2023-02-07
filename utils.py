e79b5156e52c5cf40b64d54b29d992447d10c29c
Luke Schiefelbein <luke.schiefelbein@gmail.com>
2023-02-07 09:31:42 -0500

    use project root

--- utils.py
+++ utils.py
@@ -1,7 +1,21 @@
 import os
+import pathlib
 
 DOCKER_DATA_PATH = "/code/data/"
-LOCAL_DATA_PATH = "./data/"
+
+
+def get_git_root():
+    last_cwd = os.getcwd()
+    while not os.path.isdir(".git"):
+        os.chdir("..")
+        cwd = os.getcwd()
+        if cwd == last_cwd:
+            raise OSError("no .git directory")
+        last_cwd = cwd
+    return last_cwd
+
+
+LOCAL_DATA_PATH = str(pathlib.Path(get_git_root()))
 
 
 def get_data_path():
